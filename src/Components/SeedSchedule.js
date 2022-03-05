5029fae6c91c291a200a57adceaece4a4e62f9f4
Grace Gardner <graceirisgardner@gmail.com>
2022-03-04 19:46:30 -0700

    conditional redering for seed schedule

--- src/Components/SeedSchedule.js
+++ src/Components/SeedSchedule.js
@@ -1,18 +1,28 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import ScheduleCard from './ScheduleCard'
 import {UserContext} from '../Contexts/UserContext'
+import {getCatalogue} from '../apiCalls'
 
 const SeedSchedule = () => {
   const {user} = useContext(UserContext)
+  const [seedsInCatalogue, setSeedsInCatalogue] = useState([])
 
   useEffect(() => {
     getCatalogue(user.token)
     .then(data => (data))
   })
 
+  const displaySchedule = (seeds) => {
+    seeds.map(seed => {
+      return (
+        <ScheduleCard seed={seed}/>
+      )
+    })
+  }
+
   return (
     <div className='schedule-container'>
-
+      {seedsInCatalogue.length < 1 && <h2>No Seeds to Plant</h2>}
     </div>
   )
 }
