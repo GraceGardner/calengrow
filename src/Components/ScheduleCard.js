b8f2da56b912917606db2df17bf99cc7ea0a2983
Grace Gardner <graceirisgardner@gmail.com>
2022-03-05 21:31:41 -0700

    generate schedule

--- src/Components/ScheduleCard.js
+++ src/Components/ScheduleCard.js
@@ -1,22 +1,23 @@
 import React, {useContext} from 'react'
 import {UserContext} from '../Contexts/UserContext'
 
-const ScheduleCard = ({card}) => {
+const ScheduleCard = (card) => {
 
-  // const displaySeedList = () => {
-  //   card.seeds.map(
-  //     return(
-  //
-  //     )
-  //   )
-  // }
+  const displaySeedsForDate = card.seeds.map(seed => {
+      return (
+        <p>{seed}</p>
+      )
+    }
+  )
 
   return (
     <div className='next-card-container'>
      <div>
       <p>{card.date}</p>
      </div>
-     <div>Seeds for planting</div>
+     <div>
+      {displaySeedsForDate}
+     </div>
     </div>
   )
 }
--- src/Components/SeedSchedule.js
+++ src/Components/SeedSchedule.js
@@ -17,17 +17,71 @@ const SeedSchedule = () => {
     }
   })
 
-  const displaySchedule =
-    seedsInCatalogue.map(seed => {
+  const formatDate = (date) => {
+    let [month, day, year] = date.split('-');
+    return [year, month, day].join('-');
+  }
+
+  const getNumberDays = (string) => {
+    return parseInt(string.slice(0, 1)) * 7
+  }
+
+  const getMultipleDays = (string) => {
+    return parseInt(string.slice(5, 6)) * 7
+  }
+
+  const changeMonthsToDays = (string) => {
+    return parseInt(string.slice(0, 1)) * 30
+  }
+
+
+ const findPlantDate = (type) => {
+  const lastFrost = formatDate(user.last_frost)
+  let plantDate;
+  if (type.planting_date.length > 15 && type.planting_date.includes('before')){
+    plantDate = - getMultipleDays(type.planting_date)
+  } else if (type.planting_date.length > 15 && type.planting_date.includes('after')){
+    plantDate = getMultipleDays(type.planting_date)
+  } else if(type.planting_date.includes('week') && type.planting_date.includes('before')){
+    plantDate = - getNumberDays(type.planting_date)
+  } else if(type.planting_date.includes('week') && type.planting_date.includes('after')){
+    plantDate = getNumberDays(type.planting_date)
+  } else if (type.planting_date.includes('month') && type.planting_date.includes('before')){
+    plantDate = - changeMonthsToDays(type.planting_date)
+  } else if (type.planting_date.includes('month') && type.planting_date.includes('after')){
+    plantDate = changeMonthsToDays(type.planting_date)
+  }
+  let plantingDatestamp = new Date(lastFrost).setDate(plantDate)
+  return new Date(plantingDatestamp)
+ }
+
+  const createSchedule = () => {
+    const result = seedsInCatalogue.reduce((acc, seed) => {
+      let date = findPlantDate(seed)
+      if(acc[date]){
+        acc[date].push(seed.name)
+      } else if (!acc[date]){
+        acc[date] = [seed.name]
+      }
+      return acc
+    }, {})
+    return result
+  }
+
+  const displaySchedule = () => {
+    const schedule = createSchedule()
+    const keys = Object.keys(schedule)
+    return keys.map(key => {
       return (
-        <ScheduleCard key={seed.id} seed={seed}/>
+        <ScheduleCard key={key} date={key} seeds={schedule[key]}/>
       )
     })
+  }
 
   return (
     <div className='schedule-container'>
       {seedsInCatalogue.length < 1 && <h2>No Seeds to Plant</h2>}
-      {seedsInCatalogue > 0 && displaySchedule}
+      {seedsInCatalogue.length > 0 && displaySchedule()}
     </div>
   )
 }
