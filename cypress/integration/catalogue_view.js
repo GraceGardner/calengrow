609b6bf7824fca7ffbe05fc1f809ed0288a0a8e9
Grace Gardner <graceirisgardner@gmail.com>
2022-03-19 11:24:40 -0600

    trying a testing update

--- cypress/integration/catalogue_view.js
+++ cypress/integration/catalogue_view.js
@@ -1,16 +1,22 @@
 describe('catalogue view', () => {
 
 
-  beforeEach(() => {
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000/')
+  //   cy.get('.login-button').click()
+  //     .get('.login-email-input').type('plantlady@test.com')
+  //     .get('.login-password-input').type('plantladyword')
+  //     .get('.submit-login-button').click()
+  //     .get('.catalogue-button').click()
+  // });
+
+  it('be able to search and add seeds to users catalogue', () => {
     cy.visit('http://localhost:3000/')
     cy.get('.login-button').click()
       .get('.login-email-input').type('plantlady@test.com')
       .get('.login-password-input').type('plantladyword')
       .get('.submit-login-button').click()
       .get('.catalogue-button').click()
-  });
-
-  it('be able to search and add seeds to users catalogue', () => {
     cy.get('.add-seed-input').type('cucumber')
       .get('.add-to-catalogue-button').eq(0).click()
       .get('.add-to-catalogue-button').eq(0).contains('HAS BEEN ADDED!')
@@ -23,6 +29,12 @@ describe('catalogue view', () => {
   })
 
   it('should make a schedule for user based on catalogue', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.login-button').click()
+      .get('.login-email-input').type('plantlady@test.com')
+      .get('.login-password-input').type('plantladyword')
+      .get('.submit-login-button').click()
+      .get('.catalogue-button').click()
     cy.get('.account-button').click()
       .get('.schedule-card-container')
       .get('.plant-button-container')
